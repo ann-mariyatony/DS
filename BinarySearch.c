d479a9337c25549695f214df8168231a19425463
Ann Mariya Tony <annmariyatony2028@cs.sjcetpalai.ac.in>
2025-10-14 14:55:22 +0530

    Update BinarySearch.c

--- BinarySearch.c
+++ BinarySearch.c
@@ -48,34 +48,114 @@ void postorder(struct Node* root) {
     }
 }
 
+int search(struct Node* root, int value) {
+    if (root == NULL) 
+        return 0;  
+    if (root->data == value) 
+        return 1;  
+    if (value < root->data) 
+        return search(root->left, value);
+    else
+        return search(root->right, value);
+}
+
+
+struct Node* findMin(struct Node* root) {
+    while (root->left != NULL)
+        root = root->left;
+    return root;
+}
+
+struct Node* deleteNode(struct Node* root, int value) {
+    if (root == NULL) return root;
+    if (value < root->data) {
+        root->left = deleteNode(root->left, value);
+    } else if (value > root->data) {
+        root->right = deleteNode(root->right, value);
+    } else {
+        
+        if (root->left == NULL) {
+            struct Node* temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            struct Node* temp = root->left;
+            free(root);
+            return temp;
+        } else {
+            struct Node* temp = findMin(root->right);
+            root->data = temp->data;
+            root->right = deleteNode(root->right, temp->data);
+        }
+    }
+    return root;
+}
+
 int main() {
     struct Node* root = NULL;
-    int choice, value,n;
-       printf("Enter Total number of value to insert: ");
-         scanf("%d", &n);
-         for(int i=1;i<=n;i++)
-         {
-         printf("Value %i:" , i);
-         scanf("%d",& value);
-          root = insert(root, value);
-        }
+    int choice, value, n, val;
     
-  printf("\nInorder Traversal: ");
-  inorder(root);
- printf("\n");
-             
-        
-printf("Preorder Traversal: ");
-preorder(root);
-printf("\n");
-            
- printf("Postorder Traversal: ");
- postorder(root);
- printf("\n");
-            
- 
+        printf("\nMenu:\n");
+        printf("1. Insert\n2. Search\n3. View\n4. Delete\n5. Exit\n");
+     while (1) {
+        printf("\nEnter your choice: ");
+        scanf("%d", &choice);
+
+        if (choice==1)
+               {
+                printf("\nEnter total number of values to insert: ");
+                scanf("%d", &n);
+                printf("Enter Values:");
+                for (int i = 1; i <= n; i++) {
+                    scanf("%d", &value);
+                    root = insert(root, value);
+                }
+                
+               }
+        else if (choice==2)
+                      {
+                        printf("\nEnter value to search: ");
+                       scanf("%d", &val);
+                       if (search(root, val))
+                          printf("%d found in BST.\n", val);
+                     else
+                             printf("%d not found in BST.\n", val);
+                       
+                      }
+         else if(choice==3)
+                { 
+                printf("\nInorder Traversal: ");
+                inorder(root);
+                printf("\nPreorder Traversal: ");
+                preorder(root);
+                printf("\nPostorder Traversal: ");
+                postorder(root);
+                printf("\n");
+                
+                }
+         else if(choice==4)
+                { 
+                printf("\nEnter value to delete: ");
+                scanf("%d", &val);
+                root = deleteNode(root, val);
+                
+                }
+         else if(choice==5)
+                { 
+                printf("\nExiting...\n");
+                exit(0);  
+                }
+           else         
+                {
+                printf("Invalid choice!\n");
+        }
+    }
+
     return 0;
 }
+
+
+
 
 
 
